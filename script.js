88cf523ce44a5ddb466685c9e026ce78a9c8cb73
Terrhy999 <thyla999@gmail.com>
2020-09-19 20:11:55 -0400

    Image Deck Menu

--- script.js
+++ script.js
@@ -12,3 +12,40 @@ function setButton(elementID) {
     suitButton.innerHTML = selectedSuit.innerHTML;
     dropDownSuit();
 }
+
+
+function zIndexLoop() {
+    let cards = document.querySelectorAll("img.deckCard");
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].style.zIndex = i;
+        cards[i].style.top = i*10+"px";
+    }
+    addEventListeners();
+}
+
+function addEventListeners() {
+    let cards = document.querySelectorAll("img.deckCard");
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("mouseenter", function(){cardHoverEnter(i)}, false);
+        cards[i].addEventListener("mouseout", function(){cardHoverExit(i), false});
+    }
+}
+
+function cardHoverExit(index) {
+    let currentIndex = index;
+    let remainingCards = document.querySelectorAll("img.deckCard");
+    for (let i = currentIndex + 1; i < remainingCards.length; i++) {
+        remainingCards[i].style.top = parseInt(remainingCards[i].style.top, 10) - 270 + "px";
+    }
+    console.log(index);
+} 
+
+function cardHoverEnter(index) {
+
+    let currentIndex = index;
+    let remainingCards = document.querySelectorAll("img.deckCard");
+    for (let i = currentIndex + 1; i < remainingCards.length; i++) {
+        remainingCards[i].style.top = parseInt(remainingCards[i].style.top, 10) + 270 + "px";
+    }
+    console.log(index);
+} 
