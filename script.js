b2519c2ffa1111bd7077ee2ed9f39fb967f4239d
Terrhy999 <thyla999@gmail.com>
2020-09-21 00:01:00 -0400

    Cleaned Up Code

--- script.js
+++ script.js
@@ -1,113 +1,46 @@
-function dropDownSuit() {
-    document.getElementById("suitDropDown").classList.toggle("hide");
-}
-
-function dropDownCard() {
-    document.getElementById("cardDropDown").classList.toggle("hide");
-}
+const CARD_PADDING = 25;
+const CARD_HEIGHT = 140;
 
 function toggleHide() {
-    let cards = document.querySelectorAll("img.card")
+    let cards = document.querySelectorAll("img.toggle")
     for(let i = 0; i < cards.length; i++) {
         cards[i].classList.toggle("hide");
     }
 }
 
-function setButton(elementID) {
-    let selectedSuit = document.getElementById(elementID);
-    let suitButton = document.getElementById("suitButton");
-    suitButton.innerHTML = selectedSuit.innerHTML;
-    dropDownSuit();
+function construct () {
+    padCards(1);
+    padCards(2);
+    padCards(3);
 }
 
-
-function zIndexLoop() {
-    let cards = document.querySelectorAll("img.deckCard");
-    let faceCards = document.querySelectorAll("img.faceCard");
-    let cardsRed = document.querySelectorAll("img.deckCardRed");
+function padCards(deckNumber) {
+    let deckDiv = document.querySelector(".deck:nth-child(" + deckNumber + ")");
+    let cards = deckDiv.querySelectorAll("img.card");
     for (let i = 0; i < cards.length; i++) {
         cards[i].style.zIndex = i;
-        cards[i].style.top = i*25+"px";
-    }
-    for (let i = 0; i < faceCards.length; i++) {
-        faceCards[i].style.zIndex = i;
-        faceCards[i].style.top = i*25 + "px";
-    }
-    for (let i = 0; i < cardsRed.length; i++) {
-        cardsRed[i].style.zIndex = i;
-        cardsRed[i].style.top = i*25 + "px";
-    }
-    addEventListeners();
-}
-
-function addEventListeners() {
-    let cards = document.querySelectorAll("img.deckCard");
-    let faceCards = document.querySelectorAll("img.faceCard");
-    let cardsRed = document.querySelectorAll("img.deckCardRed");
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].addEventListener("mouseenter", function(){cardHoverEnter(i)}, false);
-        cards[i].addEventListener("mouseout", function(){cardHoverExit(i)}, false);
-    }
-    for (let i = 0; i < faceCards.length; i++) {
-        faceCards[i].addEventListener("mouseenter", function(){faceHoverEnter(i)}, false);
-        faceCards[i].addEventListener("mouseout", function(){faceHoverExit(i)}, false);
-    }
-    for (let i = 0; i < cardsRed.length; i++) {
-        cardsRed[i].addEventListener("mouseenter", function(){redHoverEnter(i)}, false);
-        cardsRed[i].addEventListener("mouseout", function(){redHoverExit(i)}, false);
+        cards[i].style.top = i * CARD_PADDING + "px";
+        cards[i].addEventListener("mouseenter", function(){hoverEnter(deckNumber, i)}, false);
+        cards[i].addEventListener("mouseout", function(){hoverExit(deckNumber, i)}, false);
     }
 }
 
-function cardHoverExit(index) {
+function hoverEnter(deckNumber, index) {
+    let deckDiv = document.querySelector(".deck:nth-child(" + deckNumber + ")");
     let currentIndex = index;
-    let remainingCards = document.querySelectorAll("img.deckCard");
-    for (let i = currentIndex + 1; i < remainingCards.length; i++) {
-        remainingCards[i].style.top = parseInt(remainingCards[i].style.top, 10) - 115 + "px";
-    }
-} 
+    let cards = deckDiv.querySelectorAll("img.card");
 
-function faceHoverExit(index) {
-    let currentIndex = index;
-    let remainingFaceCards = document.querySelectorAll("img.faceCard");
-    for (let i = currentIndex + 1; i < remainingFaceCards.length; i++) {
-        remainingFaceCards[i].style.top = parseInt(remainingFaceCards[i].style.top, 10) - 115 + "px";
+    for(let i = currentIndex + 1; i < cards.length; i++) {
+        cards[i].style.top = parseInt(cards[i].style.top, 10) + (CARD_HEIGHT - CARD_PADDING) + "px";
     }
-} 
-
-function cardHoverEnter(index) {
-
-    let currentIndex = index;
-    let remainingCards = document.querySelectorAll("img.deckCard")
-    for (let i = currentIndex + 1; i < remainingCards.length; i++) {
-        remainingCards[i].style.top = parseInt(remainingCards[i].style.top, 10) + 115 + "px";
-    }
-    
 }
 
-function redHoverEnter(index) {
-
+function hoverExit(deckNumber, index) {
+    let deckDiv = document.querySelector(".deck:nth-child(" + deckNumber + ")");
     let currentIndex = index;
-    let remainingCards = document.querySelectorAll("img.deckCardRed")
-    for (let i = currentIndex + 1; i < remainingCards.length; i++) {
-        remainingCards[i].style.top = parseInt(remainingCards[i].style.top, 10) + 115 + "px";
+    let cards = deckDiv.querySelectorAll("img.card");
+
+    for(let i = currentIndex + 1; i < cards.length; i++) {
+        cards[i].style.top = parseInt(cards[i].style.top, 10) - (CARD_HEIGHT - CARD_PADDING) + "px";
     }
-    
 }
-
-function redHoverExit(index) {
-    let currentIndex = index;
-    let remainingCards = document.querySelectorAll("img.deckCardRed");
-    for (let i = currentIndex + 1; i < remainingCards.length; i++) {
-        remainingCards[i].style.top = parseInt(remainingCards[i].style.top, 10) - 115 + "px";
-    }
-} 
-
-function faceHoverEnter(index) {
-
-    let currentIndex = index;
-    let remainingFaceCards = document.querySelectorAll("img.faceCard")
-    for (let i = currentIndex + 1; i < remainingFaceCards.length; i++) {
-        remainingFaceCards[i].style.top = parseInt(remainingFaceCards[i].style.top, 10) + 115 + "px";
-    }
-    
-} 
