2f346c1ecd4e2b4a72883e28e9fde053e1fa26cc
Terrhy999 <thyla999@gmail.com>
2020-09-22 15:02:03 -0400

    almost working select effect

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const CARD_PADDING = 25;
 const CARD_HEIGHT = 140;
+const selectionCards = document.querySelectorAll(".deck .card");
+const selectionCardsArray = Array.from(selectionCards);
+
 
 //Adds the "hide" class to all images with the class "toggle". The "hide" class sets the display to "none".
 function toggleHide() {
     let cards = document.querySelectorAll(".toggle");
     for(let i = 0; i < cards.length; i++) {
         cards[i].classList.toggle("hide");
+    }
+}
+
+function trackCards() {
+    console.log(selectionCardsArray);
+    for(let i = 0; i < selectionCards.length; i++) {
+        selectionCards[i].addEventListener("click", () => setClicked(i), false);
     }
 }
 
@@ -16,12 +26,17 @@ function toggleFlip() {
     }
 }
 
+function setClicked(i) {
+    selectionCardsArray[i].classList.toggle("clicked");
+}
+
 
 function construct () {
     padCards(1);
     padCards(2);
     padCards(3);
     toggleFlip();
+    trackCards();
 }
 
 //"deckNumber" paramater is the nth-child with the "deck" class of an element. "cards" is all of the card images inside of that "deck" element
@@ -35,7 +50,6 @@ function padCards(deckNumber) {
         cards[i].style.top = i * CARD_PADDING + "px";
         cards[i].addEventListener("mouseenter", () => { hoverEnter(deckNumber, i); }, false);
         cards[i].addEventListener("mouseout", () => hoverExit(deckNumber, i), false);
-        cards[i].addEventListener("click", function () {console.log(this.style.zIndex)}, false);
     }
 }
 
@@ -49,7 +63,12 @@ function hoverEnter(deckNumber, index) {
     let cards = deckDiv.querySelectorAll("img.card");
 
     for(let i = currentIndex + 1; i < cards.length; i++) {
-        cards[i].style.top = parseInt(cards[i].style.top, 10) + (CARD_HEIGHT - CARD_PADDING) + "px";
+        
+        if (cards[i-1].classList.contains("clicked")) {
+            return;
+        } else {
+            cards[i].style.top = parseInt(cards[i].style.top, 10) + (CARD_HEIGHT - CARD_PADDING) + "px";
+        }
     }
 }
 
@@ -62,7 +81,12 @@ let hoverExit = function (deckNumber, index) {
     let cards = deckDiv.querySelectorAll("img.card");
 
     for(let i = currentIndex + 1; i < cards.length; i++) {
-        cards[i].style.top = parseInt(cards[i].style.top, 10) - (CARD_HEIGHT - CARD_PADDING) + "px";
+
+        if (cards[i-1].classList.contains("clicked")) {
+            return;
+        } else {
+            cards[i].style.top = parseInt(cards[i].style.top, 10) - (CARD_HEIGHT - CARD_PADDING) + "px";
+        }
     }
 }
 
